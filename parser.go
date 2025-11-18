0ff65c135adc291a18bea46895c50b2ed13176f0
Tyler Arciniaga <tylerarc@umich.edu>
2025-11-17 19:10:41 -0500

    chore: refactored tcp stream parsing logic to be more robust and handle pipelined commands, also enforced seperation of concerns by making a parser struct that only handles parsing when given a slice of bytes

---
+++ parser.go
@@ -0,0 +1,63 @@
+package main
+
+import "strconv"
+
+type Parser struct{}
+
+func (p Parser) ReadLine(buf []byte) ([]byte, int, bool) {
+	var line []byte
+	for i, v := range buf {
+		if v == '\r' && (i+1) < len(buf) && buf[i+1] == '\n' { // delimiter found...
+			line = buf[:i]           //extract chunk of slice up to but not including delimter
+			return line, i + 2, true //TODO may need to return i + 1 not i + 2!!!
+		}
+	}
+	return nil, 0, false
+}
+
+func (p Parser) TryParsingCommand(buf []byte) ([][]byte, int, bool) {
+	var cmd [][]byte
+	offset := 0
+	line, consumed, ok := p.ReadLine(buf[offset:])
+	if !ok {
+		return nil, 0, false
+	}
+
+	offset += consumed
+	cmdArrayLen := p.ReadPrefixLength(line)
+
+	for i := 0; i < cmdArrayLen; i++ {
+		line, consumed, ok := p.ReadLine(buf[offset:])
+		if !ok {
+			return nil, 0, false
+		}
+		offset += consumed
+		prefixLen := p.ReadPrefixLength(line)
+		if len(buf[offset:]) < prefixLen+2 {
+			return nil, 0, false
+		}
+
+		bulkString := buf[offset : offset+prefixLen]
+		offset += prefixLen + 2
+
+		cmd = append(cmd, bulkString)
+
+	}
+	return cmd, offset, true
+}
+
+func (p Parser) ReadPrefixLength(b []byte) int {
+
+	var numElements int
+	multiplier := 1
+
+	for i := len(b) - 1; i >= 0; i-- {
+		digit, err := strconv.Atoi(string(b[i]))
+		if err != nil {
+			continue
+		}
+		numElements += (digit * multiplier)
+		multiplier *= 10
+	}
+	return numElements
+}
--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Server struct {
+	Parser    Parser
 	connSet   map[net.Conn]bool
 	joinChan  chan (net.Conn)
 	leaveChan chan (net.Conn)
@@ -51,107 +52,23 @@ func (s *Server) DisconnectConnections() {
 	}
 }
 
-func (s *Server) ReadLine(buf []byte) ([]byte, int, bool) {
-	var line []byte
-	for i, v := range buf {
-		if v == '\r' && (i+1) < len(buf) && buf[i+1] == '\n' { // delimiter found...
-			line = buf[:i] //extract chunk of slice up to but not including delimter
-			return line, i, true
-		}
-	}
-	return nil, 0, false
-}
-
-func (s *Server) ReadBulkString(l int, buf []byte) ([]byte, int) {
-	var bytes []byte
-	for i, v := range buf {
-		if v == '\r' && (i+1) < len(buf) && buf[i+1] == '\n' { // delimiter found...
-			bytes = buf[:i] //extract chunk of slice up to but not including delimter
-			return bytes, i
-		}
-	}
-
-	return nil, 0 //should never be reached
-}
-
 func (s *Server) HandleClientStream(conn net.Conn) {
-	buf := make([]byte, 1024)
-	newChunk := make([]byte, 1024)
+	buf := make([]byte, 4096)
+	temp := make([]byte, 4096)
 
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
 			slog.Error(err.Error())
+			s.leaveChan <- conn
+		}
+
+		buf = append(buf, temp[:n]...)
+		cmd, consumed, ok := s.Parser.TryParsingCommand(buf)
+		if !ok {
 			break
 		}
-		buf = buf[:n]
-
-		var cmd [][]byte
-
-		//Read up until first delimeter
-		line, i, ok := s.ReadLine(buf)
-		for !ok {
-			//read in new chunk and append to buffer
-			n, err := conn.Read(newChunk)
-			if err != nil {
-				slog.Error(err.Error())
-				s.leaveChan <- conn
-				return
-			}
-			buf = append(buf, newChunk[:n]...)
-			line, i, ok = s.ReadLine(buf)
-		}
-
-		cmdArrayLen := s.ReadPrefixLength(line) //determine the array length
-
-		if i+2 >= len(buf) {
-			buf = []byte{}
-		} else {
-			buf = buf[i+2:]
-		}
-
-		for range cmdArrayLen {
-			line, i, ok := s.ReadLine(buf)
-			for !ok {
-				n, err := conn.Read(newChunk)
-				if err != nil {
-					slog.Error(err.Error())
-					s.leaveChan <- conn
-					return
-				}
-				buf = append(buf, newChunk[:n]...)
-				line, i, ok = s.ReadLine(buf)
-			}
-
-			prefixLen := s.ReadPrefixLength(line) //determine the bulk string length
-
-			if i+2 >= len(buf) {
-				buf = []byte{}
-			} else {
-				buf = buf[i+2:]
-			}
-
-			if len(buf) < prefixLen {
-				//read in new chunk and append to buffer here
-				n, err := conn.Read(newChunk)
-				if err != nil {
-					slog.Error(err.Error())
-					s.leaveChan <- conn
-					return
-				}
-				buf = append(buf, newChunk[:n]...)
-			}
-
-			bytes, i := s.ReadBulkString(prefixLen, buf)
-			cmd = append(cmd, bytes)
-
-			if i+2 >= len(buf) {
-				buf = []byte{}
-			} else {
-				buf = buf[i+2:]
-			}
-
-		}
+		buf = buf[consumed:]
 
 		resp := s.HandleParsedCommands(cmd)
 		conn.Write(resp)
@@ -190,24 +107,4 @@ func (s *Server) GenerateSimpleString(bytes []byte) []byte {
 	out = append(out, bytes...)
 	out = append(out, '\r', '\n')
 	return out
-}
-
-func (s *Server) ReadPrefixLength(b []byte) int {
-	//reverse byte slice to make digit math work
-	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
-	}
-
-	var numElements int
-	multiplier := 1
-
-	for _, v := range b {
-		digit, err := strconv.Atoi(string(v))
-		if err != nil {
-			continue
-		}
-		numElements += (digit * multiplier)
-		multiplier *= 10
-	}
-	return numElements
 }
